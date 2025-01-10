d9726c04ed7db779f0c6453b199619ca3cee79ba
Jorge <erudito.tv@gmail.com>
2025-01-10 10:12:53 -0500

    v1

---
+++ coneccion.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func abrirConexion() {
+	// Datos de conexión
+	dsn := "root:Alfayomega1232*@tcp(127.0.0.1:5588)/1010269579_db0000000001"
+
+	// Conexión a la base de datos
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Verifica la conexión
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Conexión exitosa a MariaDB")
+}
+
+func Saludo() {
+	println("dfdf")
+}
---
+++ pdf.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jung-kurt/gofpdf"
+)
+
+func generarPDF() {
+	// Crear una instancia de PDF
+	pdf := gofpdf.New("P", "mm", "A4", "")
+
+	// Agregar una página
+	pdf.AddPage()
+
+	// Establecer la fuente
+	pdf.SetFont("Arial", "B", 16)
+
+	// Agregar un título
+	pdf.Cell(40, 10, "Hola, este es un PDF creado en Go!")
+
+	// Guardar el PDF en un archivo
+	err := pdf.OutputFileAndClose("mi_pdf.pdf")
+	if err != nil {
+		fmt.Println("Error al generar el PDF:", err)
+		return
+	}
+
+	fmt.Println("PDF generado exitosamente como 'mi_pdf.pdf'")
+}

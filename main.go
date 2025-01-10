954ec4e6ad35381751e64f587c45589ba3946896
Jorge <erudito.tv@gmail.com>
2025-01-10 13:20:08 -0500

    v1

--- main.go
+++ main.go
@@ -1,6 +1,31 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
-	GenerarPDF()
+	valor1 := GenerarPDF_Bytes()
+	valor2 := GenerarPDF_Base64()
+
+	//println("PDF generado con tamaño: %d bytes\n", len(valor1))
 	//ConexionDB()
+
+	app := fiber.New()
+	// Ruta para retornar bytes
+	app.Get("/bytes", func(c *fiber.Ctx) error {
+		c.Set("Content-Type", "application/pdf")
+		c.Set("Content-Disposition", "inline; filename=\"document.pdf\"")
+		return c.Send(valor1)
+	})
+
+	// Ruta para retornar base64
+	app.Get("/base64", func(c *fiber.Ctx) error {
+
+		return c.SendString(valor2)
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
--- pdf.go
+++ pdf.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/base64"
 
 	"github.com/jung-kurt/gofpdf"
 )
 
-func GenerarPDF() {
+func GenerarPDF_Bytes() []byte {
 	// Crear una instancia de PDF
 	pdf := gofpdf.New("P", "mm", "A4", "")
 
@@ -19,12 +20,44 @@ func GenerarPDF() {
 	// Agregar un título
 	pdf.Cell(40, 10, "Hola, este es un PDF creado en Go!")
 
-	// Guardar el PDF en un archivo
-	err := pdf.OutputFileAndClose("mi_pdf.pdf")
+	// Crear un buffer para almacenar el PDF
+	var buffer bytes.Buffer
+
+	// Guardar el contenido del PDF en el buffer
+	err := pdf.Output(&buffer)
 	if err != nil {
-		fmt.Println("Error al generar el PDF:", err)
-		return
+		panic(err) // Maneja el error según sea necesario
 	}
 
-	fmt.Println("PDF generado exitosamente como 'mi_pdf.pdf'")
+	// Retornar los bytes del PDF
+	return buffer.Bytes()
+}
+
+func GenerarPDF_Base64() string {
+	// Crear una instancia de PDF
+	pdf := gofpdf.New("P", "mm", "A4", "")
+
+	// Agregar una página
+	pdf.AddPage()
+
+	// Establecer la fuente
+	pdf.SetFont("Arial", "B", 16)
+
+	// Agregar un título
+	pdf.Cell(40, 10, "Hola, este es un PDF creado en Go!")
+
+	var buffer bytes.Buffer
+
+	// Guardar el contenido del PDF en el buffer
+	err := pdf.Output(&buffer)
+	if err != nil {
+		panic(err) // Maneja el error según sea necesario
+	}
+
+	// Convertir el buffer a Base64
+	base64PDF := base64.StdEncoding.EncodeToString(buffer.Bytes())
+
+	// Retornar el PDF en formato Base64
+	return base64PDF
+
 }
